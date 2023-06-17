9a3a5f63bfc97088126fdcf1b4608832836d7c39
Aki <msf5pbsi@gmail.com>
2023-06-17 21:53:15 +0900

    fixed bugs

--- init.sh
+++ init.sh
@@ -1,23 +1,73 @@
 sudo yum install -y nodejs
+
+if [$? -ne 0]; then
+    exit 1
+fi
+
 sudo yum install -y python3-pip
+
+if [$? -ne 0]; then
+    exit 1
+fi
 
 cd /opt/aws-sample-project
 
-python3 -m venv venv
+if [$? -ne 0]; then
+    exit 1
+fi
+
+sudo python3 -m venv venv
+
+if [$? -ne 0]; then
+    exit 1
+fi
 
 cd venv/bin
 
+if [$? -ne 0]; then
+    exit 1
+fi
+
 source activate
+
+if [$? -ne 0]; then
+    exit 1
+fi
 
 cd /opt/aws-sample-project/frontend
 
+if [$? -ne 0]; then
+    exit 1
+fi
+
 npm install
+
+if [$? -ne 0]; then
+    exit 1
+fi
 
 npm run build
 
+if [$? -ne 0]; then
+    exit 1
+fi
+
 python3 -m pip install --upgrade pip
 
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
-pip3 install bcrypt boto3 opencv-python Flask gunicorn ultralytics 
+if [$? -ne 0]; then
+    exit 1
+fi
+
+pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cpu
+
+if [$? -ne 0]; then
+    exit 1
+fi
+
+pip3 install bcrypt boto3 opencv-python Flask gunicorn ultralytics
+
+if [$? -ne 0]; then
+    exit 1
+fi
 
 cd /opt/aws-sample-project/
