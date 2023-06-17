53fcb634da35509f798895683445ad1a68bda8c0
Aki <msf5pbsi@gmail.com>
2023-06-17 19:48:23 +0900

    added a bash script

---
+++ init.sh
@@ -0,0 +1,21 @@
+sudo yum install -y nodejs
+sudo yum install -y python3-pip
+
+cd /opt/aws-sample-project
+
+python3 -m venv venv
+
+cd venv/bin
+
+source activate
+
+cd /opt/aws-sample-project/frontend
+
+npm install
+
+npm run build
+
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+pip3 install bcrypt, boto3, opencv-python, Flask, gunicorn ultralytics 
+
+cd /opt/aws-sample-project/
